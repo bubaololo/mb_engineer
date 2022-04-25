87d6f50df68bd79327005cbf934d48204c0ca028
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-04-25 16:50:05 +0200

    desktop ready

--- js/index.js
+++ js/index.js
@@ -18,6 +18,9 @@ var swiper = new Swiper(".mySwiper", {
 
 
 const bar = document.querySelector(".quest__slider_bar_wrapper");
+if (bar) {
+
+  
 const bullets = bar.children;
 
 
@@ -57,12 +60,69 @@ const observer = new MutationObserver(function (mutations) {
     let currentBullet = findActiveBullet(bullets);
     removeAdditionClasses(bullets);
     findChecked(bullets, currentBullet);
+    if (bullets[currentBullet + 1] !== undefined) {
     bullets[currentBullet + 1].classList.add("bullet_next_next");
+    }
   });
 });
 
-
 const config = { attributes: true, childList: true, characterData: true };
 
-
 observer.observe(target, config);
+
+const successMessage = document.querySelector('.quest .success');
+
+function send(event) {
+  event.preventDefault()
+  successMessage.classList.add('active');
+}
+}
+
+
+
+var swiper = new Swiper(".manufSwiper", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".manufSwiperTop", {
+  spaceBetween: 10,
+
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper3 = new Swiper(".projects__top_Swiper", {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".projects__top_next",
+    prevEl: ".projects__top_prev",
+  },
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper4 = new Swiper(".projects__bot_Swiper", {
+  pagination: {
+    el: ".projects__bot_slider_paginate",
+    type: "bullets",
+    // currentClass: "quest__slider_dot",
+    // progressbarFillClass: ".swiper-pagination-bullet-active",
+  },
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".projects__bot_next",
+    prevEl: ".projects__bot_prev",
+  },
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
